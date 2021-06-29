2b3e013ab183a7bfe058aa1e8ff062f036beaa6f
Ueda <to24toro@gmail.com>
2021-06-29 22:11:52 +0900

    6/29

--- function/LCS.py
+++ function/LCS.py
@@ -2,31 +2,33 @@
 #例) "abcdefg" と "cdeg" が与えられたとして, 最大の部分文字列は "cde" なので答えは３.
 
 def LCS(s1,s2):
-    if not s1 or not s2:
-        return 0
-    len1, len2 = len(s1), len(s2)
+    len1,len2 = len(s1),len(s2)
+    if len1 ==0 or len2 ==0: return 0
 
     m = [[0 for _ in range(len2)] for _ in range(len1)]
-
-    lcs = 0
-
-    for i,x in enumerate(s1):
-        if x == s2[0]:
+    max_len = 0
+    isSameFound = False
+    for i in range(len1):
+        if isSameFound or s1[i] == s2[0]:
             m[i][0] = 1
-            lcs = 1
-    for i,x in enumerate(s2):
-        if x == s1[0]:
+            isSameFound = True
+        max_len = max(max_len,m[i][0])
+
+    isSameFound = False
+    for i in range(len2):
+        if isSameFound or s2[i] == s1[0]:
             m[0][i] = 1
-            lcs = 1
-    
-    for i in range(1,len1):
-        for j in range(1,len2):
-            if s1[i] == s2[j]:
-                m[i][j] = m[i-1][j-1] + 1
-                lcs = max(m[i][j],lcs)
+            isSameFound = True
+        max_len = max(max_len,m[0][i])
+
+    for i in range(0,len1-1):
+        for j in range(0, len2-1):
+            if s1[i+1] == s2[j+1]:
+                m[i+1][j+1] = m[i][j]+1
             else:
-                m[i][j] = 0
-    return lcs
+                m[i+1][j+1] = max(m[i+1][j], m[i][j+1])
+            max_len = max(max_len, m[i+1][j+1])
+    return m
 
 
 #文字列S1とS2の最長共通部分を求めよ. 最長共通部分とは,
