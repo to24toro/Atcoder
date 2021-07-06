dd378948f6864703598de8ecdd385729676ab59a
to24toro <to24toro@gmail.com>
2021-07-06 10:07:10 +0900

    7/6

--- ABC023/D.py
+++ ABC023/D.py
@@ -5,3 +5,29 @@ from heapq import *
 import math
 import sys
 sys.setrecursionlimit(1<<20)
+
+n = int(input())
+X = [tuple(map(int,input().split())) for _ in range(n)]
+
+l,r = 0,10**18+1
+
+while r-l>1:
+    m = (r+l)//2
+    t = 0
+    f = True
+    T = []
+    for h,s in X:
+        T.append((m-h)//s)
+    T.sort()
+    for i,t in enumerate(T):
+        if t<0:
+            f = False
+            break
+        if i>t:
+            f = False
+            break 
+    if not f:
+        l = m
+    else:
+        r = m
+print(r)
--- ABC146/C.py
+++ ABC146/C.py
@@ -1,17 +1,21 @@
 from itertools import *
 from collections import *
 from heapq import *
-from heapq import *
+from bisect import *
+from copy import *
 import math
 import sys
 sys.setrecursionlimit(1<<20)
+INF = float('inf')
 
-n = int(input())
-l,r = 0,10**18
+a,b,x = map(int,input().split())
+
+l,r = 0,min(x+1,10**9+1)
 while r-l>1:
     m = (r+l)//2
-    if m*(m+1)//2<=n+1:
+    cost = a*m+b*int(len(str(m)))
+    if cost<=x:
         l = m
     else:
         r = m
-print(1+n-l)
+print(l)
